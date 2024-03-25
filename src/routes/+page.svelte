058cb5d5753b8223cbd37679537fdd5bbf3bdc0a
Mendel Cyprys <mmcyprys@gmail.com>
2024-03-25 02:44:47 +0000

    First commit

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,5 +1,11 @@
 <script lang="ts">
-    import katex from 'katex';
+  import katex from "katex";
 </script>
 
-<p>{@html katex.renderToString("\\int^{\\infty}_{-\\infty} \\exp\\left(-x^2\\right) \\cdot \\sin(x) \\ \\text{d}x", { displayMode: true })}</p>
+<div class="text-3xl">
+  {@html katex.renderToString(
+    String.raw`\int^{\infty}_{-\infty} \exp \left( -x^2 \right) \cdot \sin(x) \ \text{d}x`,
+    { displayMode: true }
+  )}
+</div>
+<h1>tailwind test</h1>
