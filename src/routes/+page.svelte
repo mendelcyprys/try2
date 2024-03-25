8527203fc85acf0770bfb0c7559dfb52dfeb5900
Mendel Cyprys <mmcyprys@gmail.com>
2024-03-25 02:36:49 +0000

    Initial commit

---
+++ src/routes/+page.svelte
@@ -0,0 +1,5 @@
+<script lang="ts">
+    import katex from 'katex';
+</script>
+
+<p>{@html katex.renderToString("\\int^{\\infty}_{-\\infty} \\exp\\left(-x^2\\right) \\cdot \\sin(x) \\ \\text{d}x", { displayMode: true })}</p>
