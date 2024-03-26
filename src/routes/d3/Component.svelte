76bb42f1f63011126d38c2df6dba761a10d35533
Mendel Cyprys <mmcyprys@gmail.com>
2024-03-26 17:32:57 +0000

    Rotating sphere

--- src/routes/d3/Component.svelte
+++ src/routes/d3/Component.svelte
@@ -1,21 +1,45 @@
 <script lang="ts">
   import * as d3 from "d3";
+  import { Slider } from "$lib/components/ui/slider";
+  import Button from "$lib/components/ui/button/button.svelte";
 
-  const projection = d3.geoOrthographic().rotate([100, -30, 0]).translate([250, 250]);
-  const path = d3.geoPath(projection);
+  let rotation: [[number], [number], [number]];
+  function initAngles() {
+    rotation = [[0], [-25.3], [0]];
+  }
+  initAngles();
+
+  $: projection = d3
+    .geoOrthographic()
+    .rotate(rotation.flat() as [number, number, number])
+    .translate([260, 260]);
+  $: path = d3.geoPath(projection);
+  const circle = d3.geoCircle();
 </script>
 
-<svg width="500" height="500" xmlns="http://www.w3.org/2000/svg">
-  <path
-    d={path({ type: "Sphere" })}
-    style="fill:none;stroke:rgba(0, 0, 0, 0.5);stroke-width:1"
-  />
-  <path
-    d={path(d3.geoGraticule10())}
-    style="fill:none;stroke:rgba(0, 0, 0, 0.2);stroke-width:1"
-  />
-  <path
-    d={path({ type: "LineString", coordinates: [[-90, 0], [0, 40], [90, 0], [180, -40], [-90, 0]]})}
-    style="fill:none;stroke:rgba(0, 0, 0, 1);stroke-width:1"
-  />
-</svg>
+<div class="flex flex-row gap-5 items-center">
+  <svg width="520" height="520" xmlns="http://www.w3.org/2000/svg">
+    <path
+      d={path({ type: "Sphere" })}
+      style="fill:none;stroke:rgba(0, 0, 0, 1);stroke-width:2"
+    />
+    <path
+      d={path(d3.geoGraticule10())}
+      style="fill:none;stroke:rgba(0, 0, 0, 0.2);stroke-width:1"
+    />
+    <path
+      d={path(circle.center([0, 90]).radius(90)())}
+      style="fill:none;stroke:rgba(0, 0, 0, 0.5);stroke-width:2"
+    />
+    <path
+      d={path(circle.center([130, 68]).radius(90)())}
+      style="fill:none;stroke:rgba(0, 0, 0, 0.5);stroke-width:2"
+    />
+  </svg>
+  <div class="flex flex-col gap-5 w-64">
+    <Slider bind:value={rotation[0]} min={-120} max={120} step={0.5} />
+    <Slider bind:value={rotation[1]} min={-120} max={120} step={0.5} />
+    <Slider bind:value={rotation[2]} min={-120} max={120} step={0.5} />
+    <Button variant="outline" on:click={initAngles}>Reset</Button>
+  </div>
+</div>
